9b7b64a74f944943fde7b55b364302ae835a2204
faustinayuliana <faustinayuliana8@gmail.com>
2026-05-09 21:41:28 +0700

    updateASINK

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,30 @@
+import { lusitana } from '@/app/ui/fonts';
+import CustomersTable from '@/app/ui/customers/table';
+import { fetchFilteredCustomers } from '@/app/lib/data';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Customers',
+};
+
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+  };
+}) {
+  const query = searchParams?.query || '';
+  
+  // Mengambil data customer dari database
+  const customers = await fetchFilteredCustomers(query);
+
+  return (
+    <div className="w-full">
+      <h1 className={`${lusitana.className} mb-8 text-xl md:text-2xl`}>
+        Customers
+      </h1>
+      <CustomersTable customers={customers} />
+    </div>
+  );
+}
