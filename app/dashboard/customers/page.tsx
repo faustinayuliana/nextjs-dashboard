cba2a317ad8cfa5913fffc7a73b1595ffda70165
faustinayuliana <faustinayuliana8@gmail.com>
2026-05-16 20:33:44 +0700

    sabar4

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -7,13 +7,12 @@ export const metadata: Metadata = {
   title: 'Customers',
 };
 
-export default async function Page({
-  searchParams,
-}: {
-  searchParams?: {
+export default async function Page(props: {
+  searchParams?: Promise<{
     query?: string;
-  };
+  }>;
 }) {
+  const searchParams = await props.searchParams;
   const query = searchParams?.query || '';
   
   const customers = await fetchFilteredCustomers(query);
